695686bce17221f9a0f65c11dd6cfdbccade0dd2
biedro94 <biedrzyckid94@gmail.com>
2017-06-03 13:11:44 +0200

    Configure logger

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -2,11 +2,20 @@ package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Marek on 14.02.2016.
  */
 public class ArrayConverter {
+	
+	private static Logger log = LogManager.getLogger(); 
+	
     public int[] convertToInts(String[] strings) {
+    	
+    	log.debug(Arrays.stream(strings).mapToInt(Integer::valueOf).toArray());   
+    	
         return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
     }
 }
